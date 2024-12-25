fca3d17becfc863882f5a2554d5fd4112025a719
VladimirKhrapovickiy <khrapovickiy.vt@gmail.com>
2024-12-25 13:41:28 +0700

    form and animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
+//popup
+    const $popupSub = $(".popup-sub");
+    const $popupClose = $(".popup-sub__close")
 
+    setTimeout(() => {
+        $popupSub.removeClass("ml-1000");
+    }, 2500);
+    $popupClose.on("click", function(){
+        $popupSub.addClass("ml-1000");
+    })
 })
